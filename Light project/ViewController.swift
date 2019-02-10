99a56d5fa675b58339ed005b7b3303d5aee4f823
apple <apple@apples-mac.local>
2019-02-10 07:01:35 -0800

    Initial Commit

---
+++ Light project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Light project
+//
+//  Created by apple on 10/02/2019.
+//  Copyright © 2019 Sergey Yakovlev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
